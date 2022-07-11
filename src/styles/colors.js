ddd04edc2e3157b3474ad8a516938c83a0e964f5
Arianna Avalos <68700321+ariavalosm@users.noreply.github.com>
2022-07-11 15:49:44 -0500

    colors added

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+export const colors = {
+  gray: {
+    dark: "#373737", /*DarkGray*/
+    medium: "#616161", /* Gray -  for shallow, use 15% */
+    light: "#8E8E8E", /*LightGray*/
+  },
+  pink: {
+    50: "#F48FB1", /* Pink -  for shallow, use 15% */
+    100: "#FFC1E3", /*LightPink*/
+    200: "#BF5F82", /* DarkPink*/
+  },
+  black: "#000000",
+  white: "#FFFFFF",
+  background: "#E1E2E1", /*background light*/
+  secondback: "#F5F5F6",
+};
