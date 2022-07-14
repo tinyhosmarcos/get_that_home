cb8b0f0a680521bc681ec4f148fae05ff1028c5b
Arianna Avalos <68700321+ariavalosm@users.noreply.github.com>
2022-07-14 15:49:17 -0500

    rent form added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import Listing from './pages/PropertyListing';
 
 function App() {
 
   return (
-
-  );
+      <Listing />
+  )
 }
 
 export default App;
--- src/pages/PropertyListing.js
+++ src/pages/PropertyListing.js
@@ -1,44 +1,45 @@
 import React from "react";
 
 const Listing = () => {
+  return(
     <div className="propContainer">
-    <form>
-      <h1>
-        Create a property listing
-      </h1>
-        <div>
-          /* Opeeration type */
-          /* Address*/
-          /* Montly Rent */
-          /* Maintanance */
-          <div id="checkbox-group">Property type</div>
-          <div role="group" aria-labelledby="checkbox-group">
-            <label>
-              <Field type="checkbox" name="checked" value="Apartment" />
-              Aparment
-            </label>
-            <label>
-              <Field type="checkbox" name="checked" value="House" />
-              House
-            </label>
-          
+      <form>
+        <h1>
+          Create a property listing
+        </h1>
+          <div className= "firstblock">
+            <label htmlFor="address">Address</label>
+            <input type="text" id="address" name="address" placeholder="start typing to autocomplete"/>
+            <label htmlFor="name">Montly Rent</label>
+            <input type="currency" id="rent" name="rent" placeholder="2000"/>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" placeholder="John Doe"/>
           </div>
-          <div>
-            /* Bedrooms - */
-            /* Bathrooms - */
-            /* Area om M2 - */
-          </div>
-          /* Pets checkbox - */
-          <p>Allowing pets increases the likehood of renters  liking the property by 9001%.
-It also makes you a better person.</p>
-          /* About thsi property- */
-          /* Pets checkbox - */
-          /* Pets checkbox - */
-          /* Pets checkbox - */
+          <div className="checkboxes">
 
-        </div>
-    </form>
+          </div>
+
+          <div className= "secondblock">
+            <label htmlFor="name">Name</label>
+            <select type="text" id="name" name="name" placeholder="John Doe"/>
+            <label htmlFor="name">Name</label>
+            <select type="text" id="name" name="name" placeholder="John Doe"/>
+            <label htmlFor="name">Name</label>
+            <select type="text" id="name" name="name" placeholder="John Doe"/>
+          </div>
+          <div className="checkboxes">
+            <p>Allowing pets increases the likehood of renters  liking the property by 9001%.
+It also makes you a better person.</p>
+          </div>
+          <div className="">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" placeholder="John Doe"/>
+            <p>Renters will read this first, so highlight any features or important information the apartment has.</p>
+          </div>
+
+      </form>
     </div>
+  )
 }
 
 export default Listing;
