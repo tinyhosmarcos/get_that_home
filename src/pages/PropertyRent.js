2d5f5d4c01586176a3027bd6f485cb31ff1eb00e
Arianna Avalos <68700321+ariavalosm@users.noreply.github.com>
2022-07-15 12:17:06 -0500

    initial values

--- src/pages/PropertyRent.js
+++ src/pages/PropertyRent.js
@@ -4,19 +4,52 @@ import "./Property.css";
 
 const ListingRent = () => {
 
-  return(
-    <div className="propContainer">
-      <Formik>
+    return(
+        <div className="propContainer">
+
+          <Formik
+             initialValues= {{
+              address: '',
+              currency: '',
+              monthlyrent:'',
+              maintanance: '',
+              appartment: '',
+              house: '',
+              area: '',
+              pets: '',
+              textarea: '',
+            }}
+            >
 
       <Form>
         <h1 className="title"> Create a property listing</h1>
           <div className= "firstblock">
             <label htmlFor="address">Address</label>
-            <input className="address" type="text" id="address" name="address" placeholder="start typing to autocomplete"/>
-            <label htmlFor="rent">Montly Rent</label>
-            <input className="currency" type="number" min="0.01" step="0.01" max="20000" placeholder="2000" />
+            <Field 
+              className="address" 
+              type="text" 
+              id="address" 
+              name="address" 
+              placeholder="start typing to autocomplete"
+              />
+            <label htmlFor="rent">Monthly Rent</label>
+            <Field
+              className="currency" 
+              id= "monthlyrent" 
+              type="number" 
+              min="0.01" 
+              step="0.01" 
+              max="20000" 
+              placeholder="2000" />
             <label htmlFor="maintanance">Maintanance</label>
-            <input className="currency" type="number" min="0.01" step="0.01" max="2500" placeholder="2000"  />
+            <Field 
+              className="currency"
+              id ="maintanance"  
+              type="number" 
+              min="0.01" 
+              step="0.01" 
+              max="2500" 
+              placeholder="2000"  />
           </div>
           
           <div className="proptitle">
@@ -24,11 +57,17 @@ const ListingRent = () => {
               </div>
             <div className="checkboxes">
                 <div className="checks">
-                  <input type="checkbox" id="appartment" name="apparment" />
+                  <Field 
+                    type="checkbox" 
+                    id="appartment" 
+                    name="apparment" />
                   <label for="appartment"> Appartment</label>
                 </div>
                 <div className="checks">
-                  <input type="checkbox" id="house" name="house" />
+                  <Field 
+                    type="checkbox" 
+                    id="house" 
+                    name="house" />
                   <label for="appartment"> House</label>
                 </div>
             </div>
@@ -39,7 +78,7 @@ const ListingRent = () => {
                      {({ field }) => (
                         <select {...field}>
                          <option value="" disabled selected hidden>select...</option>
-                           {[1,2,3,4,5].map(i => 
+                           {[1,2,3,4,5,6].map(i => 
                               <option key={i} value={i}>{i}</option>
                                   )}
                   </select>
@@ -53,7 +92,7 @@ const ListingRent = () => {
                      {({ field }) => (
                         <select {...field}>
                          <option value="" disabled selected hidden>select...</option>
-                           {[1,2,3,4,5].map(i => 
+                           {[1,2,3,4,5,6].map(i => 
                               <option key={i} value={i}>{i}</option>
                                   )}
                   </select>
@@ -63,21 +102,35 @@ const ListingRent = () => {
 
               <div>
                 <label htmlFor="name">Area in M2</label>
-                <input className="area" type="number" min="0.01" step="0.01" max="2000" placeholder="##" />
+                <Field 
+                  className="area" 
+                  id= "area"
+                  type="number" 
+                  min="0.01" 
+                  step="0.01" 
+                  max="20000" 
+                  placeholder="##" />
               </div>
             </div>
 
           <div className="petbox">
            <div className="petcheck">
-              <input type="checkbox" id="pets" name="pets" />
-              <label for="pets"> Pets allowed</label>
+              <Field 
+                type="checkbox" 
+                id="pets" 
+                name="pets" />
+              <label htmlFor="pets"> Pets allowed</label>
             </div>
             <p className="quote">Allowing pets increases the likehood of renters  liking the property by 9001%. It also makes you a better person.</p>
           </div>
 
           <div className="textitems">
             <label htmlFor="name">About this property</label>
-            <textarea className="paratext" type="textarea" id="textarea" placeholder="My appartment is great because..."/>
+            <textarea 
+              className="paratext" 
+              type="textarea" 
+              id="textarea" 
+              placeholder="My appartment is great because..."/>
             <p>Renters will read this first, so highlight any features or important information the apartment has.</p>
           </div>
           
@@ -96,8 +149,6 @@ const ListingRent = () => {
               </div>
             </div>
                   <p>here goes a button with emotion</p>
-    
-
       </Form>
       </Formik>
     </div>
