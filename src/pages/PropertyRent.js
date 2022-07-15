e8bc7ccb015955efeda72ab4a75b077fa45f7527
Arianna Avalos <68700321+ariavalosm@users.noreply.github.com>
2022-07-15 12:51:44 -0500

    buttons

--- src/pages/PropertyRent.js
+++ src/pages/PropertyRent.js
@@ -4,23 +4,21 @@ import "./Property.css";
 
 const ListingRent = () => {
 
-    return(
-        <div className="propContainer">
-
-          <Formik
-             initialValues= {{
-              address: '',
-              currency: '',
-              monthlyrent:'',
-              maintanance: '',
-              appartment: '',
-              house: '',
-              area: '',
-              pets: '',
-              textarea: '',
-            }}
-            >
-
+  return(
+    <div className="propContainer">
+      <Formik
+        initialValues= {{
+          address: '',
+          currency: '',
+          monthlyrent:'',
+          maintanance: '',
+          appartment: '',
+          house: '',
+          area: '',
+          pets: '',
+          textarea: '',
+        }}
+      >
       <Form>
         <h1 className="title"> Create a property listing</h1>
           <div className= "firstblock">
@@ -31,7 +29,7 @@ const ListingRent = () => {
               id="address" 
               name="address" 
               placeholder="start typing to autocomplete"
-              />
+            />
             <label htmlFor="rent">Monthly Rent</label>
             <Field
               className="currency" 
@@ -40,7 +38,8 @@ const ListingRent = () => {
               min="0.01" 
               step="0.01" 
               max="20000" 
-              placeholder="2000" />
+              placeholder="2000" 
+            />
             <label htmlFor="maintanance">Maintanance</label>
             <Field 
               className="currency"
@@ -49,80 +48,82 @@ const ListingRent = () => {
               min="0.01" 
               step="0.01" 
               max="2500" 
-              placeholder="2000"  />
+              placeholder="2000"  
+            />
           </div>
           
           <div className="proptitle">
-                <h4>Property Type</h4>
-              </div>
+            <h4>Property Type</h4>
+          </div>
             <div className="checkboxes">
-                <div className="checks">
-                  <Field 
-                    type="checkbox" 
-                    id="appartment" 
-                    name="apparment" />
-                  <label for="appartment"> Appartment</label>
-                </div>
-                <div className="checks">
-                  <Field 
-                    type="checkbox" 
-                    id="house" 
-                    name="house" />
-                  <label for="appartment"> House</label>
-                </div>
+              <div className="checks">
+                <Field 
+                  type="checkbox" 
+                  id="appartment" 
+                  name="apparment" 
+                />
+                <label for="appartment"> Appartment</label>
+              </div>
+              <div className="checks">
+                <Field 
+                  type="checkbox" 
+                  id="house" 
+                  name="house" 
+                />
+                <label for="appartment"> House</label>
+              </div>
             </div>
 
           <div className= "secondblock">
             <div>
-                  <Field name="Bedrooms">
-                     {({ field }) => (
-                        <select {...field}>
-                         <option value="" disabled selected hidden>select...</option>
-                           {[1,2,3,4,5,6].map(i => 
-                              <option key={i} value={i}>{i}</option>
-                                  )}
+               <Field name="Bedrooms">
+                {({ field }) => (
+                  <select {...field}>
+                    <option value="" disabled selected hidden>select...</option>
+                      {[1,2,3,4,5,6].map(i => 
+                      <option key={i} value={i}>{i}</option>
+                      )}
                   </select>
              )}
              </Field>
-
-                </div>
-
-                <div>
-                <Field name="Bathroom">
-                     {({ field }) => (
-                        <select {...field}>
-                         <option value="" disabled selected hidden>select...</option>
-                           {[1,2,3,4,5,6].map(i => 
-                              <option key={i} value={i}>{i}</option>
-                                  )}
-                  </select>
-                    )}
-                    </Field>
-              </div>
-
-              <div>
-                <label htmlFor="name">Area in M2</label>
-                <Field 
-                  className="area" 
-                  id= "area"
-                  type="number" 
-                  min="0.01" 
-                  step="0.01" 
-                  max="20000" 
-                  placeholder="##" />
-              </div>
             </div>
+            <div>
+              <Field name="Bathroom">
+                {({ field }) => (
+                  <select {...field}>
+                    <option value="" disabled selected hidden>select...</option>
+                    {[1,2,3,4,5,6].map(i => 
+                    <option key={i} value={i}>{i}</option>
+                    )}
+                  </select>
+                )}
+              </Field>
+            </div>
+            <div>
+              <label htmlFor="name">Area in M2</label>
+              <Field 
+                className="area" 
+                id= "area"
+                type="number" 
+                min="0.01" 
+                step="0.01" 
+                max="20000" 
+                placeholder="##" 
+              />
+            </div>
+          </div>
 
           <div className="petbox">
            <div className="petcheck">
               <Field 
                 type="checkbox" 
                 id="pets" 
-                name="pets" />
+                name="pets" 
+              />
               <label htmlFor="pets"> Pets allowed</label>
             </div>
-            <p className="quote">Allowing pets increases the likehood of renters  liking the property by 9001%. It also makes you a better person.</p>
-          </div>
+              <p className="quote">Allowing pets increases the likehood of renters  liking the property by 9001%. It also makes you a better person.</p>
+            </div>
 
           <div className="textitems">
             <label htmlFor="name">About this property</label>
@@ -130,16 +131,17 @@ const ListingRent = () => {
               className="paratext" 
               type="textarea" 
               id="textarea" 
-              placeholder="My appartment is great because..."/>
-            <p>Renters will read this first, so highlight any features or important information the apartment has.</p>
+              placeholder="My appartment is great because..."
+            />
+              <p>Renters will read this first, so highlight any features or important information the apartment has.</p>
           </div>
           
           <div className="photos">
-              <h3>Photos</h3>
-              <p>Upload as many photos as you wish</p>
-            <div className="subphotos">
-                  <button>choose</button>
-                  <p className="">No file chosen</p>
+            <h3>Photos</h3>
+            <p>Upload as many photos as you wish</p>
+              <div className="subphotos">
+              <Button type="submit" icon={<RiUserReceivedLine/>} color={'primary'}>choose</Button>
+                <p className="">No file chosen</p>
                 </div>
               <p>Only images, max 5MB</p>
               <div className="uploadbox">
