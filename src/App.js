aae3aedbfd4244e36837ba65dbfa41bf7089c4d1
Julio Bazalar <jbzuloaga@gmail.com>
2022-07-15 15:23:12 -0500

    ADD: routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Listing from './pages/PropertyRent';
+import Landpage from './pages/Landpage';
+import PropertyRent from './pages/PropertyRent';
+import Login from './pages/login';
+import Signup from './pages/signup';
+import SignupCard from './pages/signup-card';
 
 function App() {
 
   return (
-      <Listing />
+    <Routes>
+      <Route path="/" element={<Landpage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup/profile" element={<SignupCard />} />
+      <Route path="/signup/type" element={<Signup />} />
+      <Route path="/property/new" element={<PropertyRent />} />
+    </Routes>
   )
 }
 
