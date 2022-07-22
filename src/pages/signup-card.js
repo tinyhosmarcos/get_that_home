5d40540106c7bdf07bf10f8353ac3c8a6adccccc
Alex Mamani <alex.mamani@tecsup.edu.pe>
2022-07-22 11:14:19 -0500

    CHORE: validations for SignUpForm

--- src/pages/signup-card.js
+++ src/pages/signup-card.js
@@ -68,35 +68,105 @@ const SignupCard = () =>{
               password: '',
               password_confirmation: '',
             }}
+
+            validate={(values) => {
+              const errors = {};
+// !!!! --->>> validaciones regex de las Values <<<---
+              const regexSentences = {
+                name: /([a-zA-Z0-9_\s]+)/,
+                email: /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
+                phone: /^[9]\d{8}$/,
+                password: /^(?=.*\d)[A-Za-z\d]{6,}$/,
+                password_confirmation: /^(?=.*\d)[A-Za-z\d]{6,}$/,
+              }
+// !!!! --->>> STATIC, no es necesario cambiar, verificarRegex es una funcion <<<---
+              function verificarObjectRegex(objeto){
+                objeto.map(texto=>{
+                if (!regexSentences[texto].test(values[texto])) {
+                  errors[texto] = "Not valid "+texto;}
+               })
+              }
+// !!!! --->>> STATIC, no es necesario cambiar (solo usa las keys del objeto Values, osea los campos) <<<---
+              verificarObjectRegex(Object.keys(values))
+// !!!! --->>> si se necesita se agrega mas Info al mensaje de error <<<---
+              function addInfoError(variable,message){
+              if (errors[variable]){
+                errors[variable]=errors[variable]+message
+                }
+              }
+              addInfoError("name",", at least 1 letter")
+              addInfoError("phone", ", They are 9 numbers")
+              addInfoError("password", ", at least 6 numbers")
+              return errors;
+            }}
+
+
+
+
+
+
+
+
+
+
             onSubmit={ async (values) => {
               await signup(values)
             }}
           >
+
+
+
+
+
+{({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          setFieldValue,
+        }) => (
+
+
+
+
            
             <Form>
               <Title>Create your account</Title>
               <div>
                 <label htmlFor="name">Name</label>
                 <Field type="text" id="name" name="name" placeholder="John Doe"/>
+                {errors.name && touched.name && 
+                <p style={{color:"red"}}>{errors.name}</p>}
               </div>
             <div>
               <label htmlFor="email"> Email</label>
               <Field type="email" id="email" name="email" placeholder="user@mail.com" />
+              {errors.email && touched.email && 
+              <p style={{color:"red"}}>{errors.email}</p>}
             </div>
             <div>
-              <label htmlFor="phone"> Phone</label>
-              <Field type="number" id="phone" name="phone" placeholder="999-999-999" />
+              <label htmlFor="phone"> Cellphone</label>
+              <Field type="number" id="phone" name="phone" placeholder="987654321" />
+              {errors.phone && touched.phone && 
+              <p style={{color:"red"}}>{errors.phone}</p>}
             </div>
             <div>
               <label htmlFor="password"> Password</label>
               <Field type="password" id="password" name="password" placeholder="******" />
+              {errors.password && touched.password && 
+              <p style={{color:"red"}}>{errors.password}</p>}
             </div>
             <div>
               <label htmlFor="password_confirmation"> Password Confirmation</label>
               <Field type="password" id="password_confirmation" name="password_confirmation" placeholder="*******" />
+              { touched['password_confirmation'] && values['password'] != values['password_confirmation'] 
+              && <p style={{color:"red"}}>Password confirmation is not the same as the original</p>}
             </div>
             <Button type="submit" color={'primary'}>Create Account</Button>
           </Form>
+                  )}
         </Formik>
       </FormContainer>
     </>
