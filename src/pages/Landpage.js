5ea57f8ff8b99f344c357fcb92de45a77d2a37b5
Julio Bazalar <jbzuloaga@gmail.com>
2022-07-22 12:43:38 -0500

    FIX: landingpage

--- src/pages/Landpage.js
+++ src/pages/Landpage.js
@@ -236,8 +236,8 @@ const Landpage = () => {
     let copy = array.slice(0);
     return function() {
       if (copy.length < 1) { copy = array.slice(0); }
-      let index = Math.floor(Math.random() * copy.length);
-      let item = copy[index];
+      const index = Math.floor(Math.random() * copy.length);
+      const item = copy[index];
       copy.splice(index, 1);
       return item
     }
